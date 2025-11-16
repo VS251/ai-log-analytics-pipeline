931ef347735a351f4dcd8061a84f46492470faab
Varun Salian <varunsalian@Varuns-MacBook-Air.local>
2025-11-16 12:10:50 +0530

    Sec: Add API Key authentication to Collector (Closes #2)

--- dummy-app/app.py
+++ dummy-app/app.py
@@ -7,15 +7,13 @@ print("Starting in 3 seconds... (Press CTRL+C to stop)")
 time.sleep(3)
 
 services = ["payment-api", "user-service", "order-service", "frontend-web", "recommendation-engine"]
-levels = ["INFO", "INFO", "INFO", "WARN", "ERROR", "DEBUG"] # More INFOs
+levels = ["INFO", "INFO", "INFO", "WARN", "ERROR", "DEBUG"] 
 messages = [
-    # Positive
     "User login successful for user_id: 123",
     "Payment processed successfully. OrderID: 456",
     "New user registered: varun@example.com",
     "Order 789 shipped.",
     "Data export job complete.",
-    # Negative
     "Payment failed: Invalid CVC.",
     "Database connection timeout.",
     "User authentication failed: Bad credentials.",
@@ -31,9 +29,11 @@ while True:
             "service": random.choice(services),
             "level": random.choice(levels),
             "message": random.choice(messages)
-        }
+        } 
 
-        response = requests.post(API_URL, json=log_entry, timeout=5)
+        headers = {"X-API-Key": "my-secret-key-123"}
+        
+        response = requests.post(API_URL, json=log_entry, headers=headers, timeout=5)
 
         if response.status_code == 200:
             print(f"Sent log: {log_entry['level']} - {log_entry['message']}")
@@ -44,7 +44,6 @@ while True:
         print(f"ERROR: Could not connect to {API_URL}. Is the Collector API running?")
     except Exception as e:
         print(f"An unknown error occurred: {e}")
-
 
     time_to_sleep = random.uniform(0.5, 3.0) 
     time.sleep(time_to_sleep)
